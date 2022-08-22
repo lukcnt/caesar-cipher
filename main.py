311e8f8dcadbf2927660a8b5729e841f7761c302
Lucas Cantoni <lukcnt@outlook.com>
2022-08-22 13:53:14 -0300

    Call the menu of the program and let the user do what they want

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ import art
 ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def menu():
+def menu_of_the_program():
     user_choice = int(input("This program encrypt or decrypt a message using the Caesar Cipher\n"
                             "Type what you want to do:\n"
                             "0 - To encrypt a message\n"
-                            "1 - To decrypt a message"))
+                            "1 - To decrypt a message\n"))
+    return user_choice
 
 print(art.logo)
-menu()
+menu = menu_of_the_program()
+if menu != 0 and menu != 1:
+    print("This option isn't valid, choose one of the listed options.")
+elif menu == 0:
+    print("Encrypt")
+else:
+    print("Decrypt")
