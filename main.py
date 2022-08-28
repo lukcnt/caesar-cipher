78707c4e0a2749d73e8bfc0c9f04f51b1aa71bd3
Lucas Cantoni <lukcnt@outlook.com>
2022-08-28 10:39:37 -0300

    Change the program menu and index checker functions for better comprehension

--- main.py
+++ main.py
@@ -3,44 +3,35 @@ import art
 ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def menu_of_the_program():
+def program_menu():
     user_choice = int(input("This program encrypt or decrypt a message using the Caesar Cipher\n"
                             "Type what you want to do:\n"
                             "0 - To encrypt a message\n"
                             "1 - To decrypt a message\n"))
     return user_choice
 
-def index_checker(letter, shift_number):
-    index_letter_encrypted = letter + shift_number
-    if index_letter_encrypted >= len(ALPHABET):
-        while index_letter_encrypted >= len(ALPHABET):
-            index_letter_encrypted -= len(ALPHABET)
-        return index_letter_encrypted
-    elif index_letter_encrypted <= (-(len(ALPHABET))):
-        while index_letter_encrypted <= (-(len(ALPHABET))):
-            index_letter_encrypted += len(ALPHABET)
-        return index_letter_encrypted
-    else:
-        return index_letter_encrypted
+def letter_index_checker(letter, shift_number, function):
 
-def encrypt(message, shift_number):
+
+def encrypt(message, shift_number, function):
     encrypted_message = ""
     for letter in message:
         if letter in ALPHABET:
-            index_shift_letter = index_checker(letter, shift_number)
+            index_shift_letter = letter_index_checker(letter, shift_number, function)
             encrypted_message += ALPHABET[index_shift_letter]
         else:
             encrypted_message += letter
     return encrypted_message
 
 print(art.logo)
-menu = menu_of_the_program()
+menu = program_menu()
 if menu != 0 and menu != 1:
     print("This option isn't valid, choose one of the listed options.")
 elif menu == 0:
+    function = "encode"
     message = input("Type your message: ").lower()
     shift_number = int(input("Type the shift number: "))
-    encrypted_message = encrypt(message, shift_number)
+    encrypted_message = encrypt(message, shift_number, function)
     print(f"Here's the encrypted message: {encrypted_message}.")
 else:
     print("Decrypt")
